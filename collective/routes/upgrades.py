bfca67099c864f622fdd40e19432221663b4802f
Nathan Van Gheem <vangheem@gmail.com>
2012-02-28 15:27:39 -0600

    fix upgrade step

--- collective/routes/upgrades.py
+++ collective/routes/upgrades.py
@@ -8,16 +8,17 @@ default_profile = 'profile-collective.routes:default'
 uninstall_profile = 'profile-collective.routes:uninstall'
 
 
-def upgrade_to_1_1a1(context):
-    context.runImportStepFromProfile(uninstall_profile, 'controlpanel')
-    context.runImportStepFromProfile(default_profile, 'controlpanel')
-    context.runImportStepFromProfile(uninstall_profile, 'propertiestool')
-    context.runImportStepFromProfile(default_profile, 'propertiestool')
-
-    # convert selected routes
+def upgrade_to_1_1(context):
+    # get activated routes before switching over
     pprops = getToolByName(context, 'portal_properties')
     props = pprops.routes_properties
     activated_routes = props.getProperty('activated_routes', ())
+
+    context.runImportStepFromProfile(uninstall_profile, 'controlpanel')
+    context.runImportStepFromProfile(uninstall_profile, 'propertiestool')
+    context.runAllImportStepsFromProfile(default_profile)
+
+    # convert selected routes
     registry = getUtility(IRegistry)
     settings = registry.forInterface(IRoutesSettings)
     settings.routes = set(activated_routes)
