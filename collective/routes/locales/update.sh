19d963cafa4e90afe9c6c94c874e97dc002e2367
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2012-03-08 00:08:56 +0100

    Removed useless row

--- collective/routes/locales/update.sh
+++ collective/routes/locales/update.sh
@@ -8,5 +8,3 @@ else
 fi
 i18ndude rebuild-pot --pot $domain.pot $MERGE --create $domain ../../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
-
-#i18ndude sync --pot plone-manual.pot */*/plone.po
