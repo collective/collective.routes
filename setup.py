25615c5e6c8412e0d7a24dd118a6d750009e5154
Gil Forcada Codinachs <gil.forcada@freitag.de>
2015-12-31 06:26:10 +0100

    txt -> rst

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ version = '1.1a2'
 setup(name='collective.routes',
       version=version,
       description="ability to add route configuration for plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
